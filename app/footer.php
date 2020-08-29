3e0a5406cdacdebc5f7a39e66a6a8957ea5f5fba
Jessica Felts <thejessicafelts@gmail.com>
2020-08-28 22:55:07 -0400

    Initial Commit for Footer Files

---
+++ app/footer.php
@@ -0,0 +1,14 @@
+        <!-- FOOTER: Global -->
+        <footer class="footer--global">
+            <div class="layout__container--fixed-width">
+                Global Footer!
+            </div>
+        </footer>
+        <!-- end FOOTER -->
+
+        <?php wp_footer(); ?>
+    </body>
+    <!-- end BODY -->
+
+</html>
+<!-- end HTML -->
