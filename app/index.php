cfcf4ae8b3dd4cf2f086c8beac4e6540034a8db3
Jessica Felts <thejessicafelts@gmail.com>
2020-08-28 22:55:36 -0400

    Initial Commit for Index Files

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+        <!-- INDEX -->
+        <section id="section--index" class="layout__page--index">
+            <div class="layout__container--fixed-width">
+                Index Page
+            </div>
+        </section>
+        <!-- end INDEX -->
+
+<?php get_footer(); ?>
