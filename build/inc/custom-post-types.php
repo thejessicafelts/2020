66edc9f8e1e0a51bff4a95a178daf7cfe93f8768
Jessica Felts <thejessicafelts@gmail.com>
2020-08-28 23:40:18 -0400

    Updating Notes for Custom Post Types file

--- build/inc/custom-post-types.php
+++ build/inc/custom-post-types.php
@@ -2,4 +2,16 @@
 
 // Include Custom Post Types Here
 
+/*
+    Custom Post Types Needed:
+        + Alerts
+        + Events
+        + Insights (Blog)
+        + Videos
+        + Recommendations
+        + Employers
+        + Education
+        + Tutorials
+*/
+
 ?>
