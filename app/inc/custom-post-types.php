9fd84cc17b5af5871a7033b8233058b29a72e20e
Jessica Felts <thejessicafelts@gmail.com>
2020-09-03 22:57:00 -0400

    Adding Education CPT to CPT File.

--- app/inc/custom-post-types.php
+++ app/inc/custom-post-types.php
@@ -55,6 +55,58 @@ function cpt_alerts() {
 } add_action('init', 'cpt_alerts', 0);
 
 // Education CPT
+function cpt_education() {
+
+    $labels = array(
+        'name'                  => _x('Education', '_themename'),
+        'singular_name'         => _x('Education', '_themename'),
+        'menu_name'             => __('Education', '_themename'),
+        'parent_item_colon'     => __('Education', '_themename'),
+        'all_items'             => __('All Education', '_themename'),
+        'view_item'             => __('View Education', '_themename'),
+        'add_new_items'         => __('Add New Education', '_themename'),
+        'add_new'               => __('Add New Education', '_themename'),
+        'edit_item'             => __('Edit Education', '_themename'),
+        'update_item'           => __('Update Education', '_themename'),
+        'search_items'          => __('Search Education', '_themename'),
+        'not_found'             => __('Education Not Found', '_themename'),
+        'not_found_in_trash'    => __('Education Not Found in Trash', '_themename'),
+    );
+    
+    $args = array(
+        'label'                 => __('education', '_themename'),
+        'description'           => __('Education', '_themename'),
+        'labels'                => $labels,
+        // Features this CPT supports in Post Editor
+        'supports'              => array( 'title',
+                                          'editor',
+                                          'excerpt',
+                                          'author',
+                                          'thumbnail',
+                                          'comments',
+                                          'revisions',
+                                          'custom-fields'
+                                        ),
+        // 'taxonomies'            => array(''),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => true,
+        'show_in_admin_bar'     => true,
+        'menu_position'         => 5,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'post',
+        'show_in_rest'          => true,
+ 
+    );
+
+    register_post_type('education', $args);
+
+} add_action('init', 'cpt_education', 0);
 
 // Employer CPT
 function cpt_employer() {
