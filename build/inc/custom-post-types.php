4c6dd14d0714d75428549eaf8b4d60d4a3186c8a
Jessica Felts <thejessicafelts@gmail.com>
2020-09-04 17:14:05 -0400

    Removing the "Posts" Default Post Type.

--- build/inc/custom-post-types.php
+++ build/inc/custom-post-types.php
@@ -1,5 +1,31 @@
 <?php
 
+/* ===========================================================================
+    Remove "Posts" Default Post Type
+=========================================================================== */
+
+// Remove side menu
+add_action('admin_menu', 'remove_default_post_type' );
+function remove_default_post_type() {
+    remove_menu_page('edit.php');
+}
+
+// Remove +New post in top Admin Menu Bar
+add_action('admin_bar_menu', 'remove_default_post_type_menu_bar', 999);
+function remove_default_post_type_menu_bar($wp_admin_bar) {
+    $wp_admin_bar->remove_node('new-post');
+}
+
+// Remove Quick Draft Dashboard Widget
+add_action('wp_dashboard_setup', 'remove_draft_widget', 999);
+function remove_draft_widget(){
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+}
+
+/* ===========================================================================
+    Add New Custom Post Types
+=========================================================================== */
+
 // Alerts CPT
 function cpt_alerts() {
 
