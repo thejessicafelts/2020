9c5246c9c50d6a9ec8d3d3aecc99b36098cf3491
Jessica Felts <thejessicafelts@gmail.com>
2020-09-06 09:56:22 -0400

    Created Changelog Template page for reference

---
+++ app/changelog.php
@@ -0,0 +1,118 @@
+<?php // Changelog Template ?>
+
+<?php get_header(); ?>
+
+        <section id="section--index" class="layout__page--index">
+            <div class="layout__container--fixed-width">
+                <section id="changelog" class="changelog">
+                    <h2>Changelog</h2>
+                    <section class="changelog__version changelog__version--1-5-2">
+                        <h3 class="changelog__version-number">1.5.2</h3>
+                        <ul class="changelog__changelog">
+                            <li class="changelog__changelog--added">
+                                <span class="changelog__changelog--tag changelog__changelog--added-tag">Added</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--changed">
+                                <span class="changelog__changelog--tag changelog__changelog--changed-tag">Changed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--fixed">
+                                <span class="changelog__changelog--tag changelog__changelog--fixed-tag">Fixed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--removed">
+                                <span class="changelog__changelog--tag changelog__changelog--removed-tag">Removed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                        </ul>
+                    </section>
+                    <section class="changelog__version changelog__version--1-5-0">
+                        <h3 class="changelog__version-number">1.5.0</h3>
+                        <ul class="changelog__changelog">
+                            <li class="changelog__changelog--added">
+                                <span class="changelog__changelog--tag changelog__changelog--added-tag">Added</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--changed">
+                                <span class="changelog__changelog--tag changelog__changelog--changed-tag">Changed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--fixed">
+                                <span class="changelog__changelog--tag changelog__changelog--fixed-tag">Fixed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--removed">
+                                <span class="changelog__changelog--tag changelog__changelog--removed-tag">Removed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                        </ul>
+                    </section>
+                    <section class="changelog__version changelog__version--1-0-0">
+                        <h3 class="changelog__version-number">1.0.0</h3>
+                        <ul class="changelog__changelog">
+                            <li class="changelog__changelog--added">
+                                <span class="changelog__changelog--tag changelog__changelog--added-tag">Added</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--changed">
+                                <span class="changelog__changelog--tag changelog__changelog--changed-tag">Changed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--fixed">
+                                <span class="changelog__changelog--tag changelog__changelog--fixed-tag">Fixed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--removed">
+                                <span class="changelog__changelog--tag changelog__changelog--removed-tag">Removed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                        </ul>
+                    </section>
+                    <section class="changelog__version changelog__version--0-2-0">
+                        <h3 class="changelog__version-number">0.2.0</h3>
+                        <ul class="changelog__changelog">
+                            <li class="changelog__changelog--added">
+                                <span class="changelog__changelog--tag changelog__changelog--added-tag">Added</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--changed">
+                                <span class="changelog__changelog--tag changelog__changelog--changed-tag">Changed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--fixed">
+                                <span class="changelog__changelog--tag changelog__changelog--fixed-tag">Fixed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--removed">
+                                <span class="changelog__changelog--tag changelog__changelog--removed-tag">Removed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                        </ul>
+                    </section>
+                    <section class="changelog__version changelog__version--0-1-0">
+                        <h3 class="changelog__version-number">0.1.0</h3>
+                        <ul class="changelog__changelog">
+                            <li class="changelog__changelog--added">
+                                <span class="changelog__changelog--tag changelog__changelog--added-tag">Added</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--changed">
+                                <span class="changelog__changelog--tag changelog__changelog--changed-tag">Changed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--fixed">
+                                <span class="changelog__changelog--tag changelog__changelog--fixed-tag">Fixed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                            <li class="changelog__changelog--removed">
+                                <span class="changelog__changelog--tag changelog__changelog--removed-tag">Removed</span>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            </li>
+                        </ul>
+                    </section>                    
+                </section>
+            </div>
+        </section>
+
+<?php get_footer(); ?>
