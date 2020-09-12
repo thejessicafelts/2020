1b1640d48c59453a8c11e7529244ab55b110fe84
Jessica Felts <thejessicafelts@gmail.com>
2020-09-11 22:55:21 -0400

    Incredibly rough beginning of The Loop

--- app/index.php
+++ app/index.php
@@ -4,7 +4,22 @@
         <section id="section--index" class="layout__page--index">
             <div class="layout__container--fixed-width">
             
-            <?php // The Loop ?>
+                <?php if(have_posts()): 
+                    while(have_posts()): the_post(); ?> 
+                        
+                        <article id="article-id-<?php the_ID(); ?>">
+                            <a href="<?php get_permalink('url'); ?>"><h2 class="post-title"><?php the_title(); ?></h2></a>
+                            <p><?php the_author(); ?> | <?php the_time(); ?></p>
+                            <p><?php the_excerpt(); ?></p>
+                            <hr>
+                            <p><?php the_content(); ?></p>
+                            <p><?php the_category(); ?> | <?php the_tags(); ?></p>
+                            <p><a href="<?php get_previous_post_link(); ?>">Previous Post</a> | <a href="<?php get_next_post_link(); ?>">Next Post</a></p>
+                        </article>
+
+                    <?php endwhile; 
+                endif; 
+                ?>
 
             </div>
         </section>
--- build/index.php
+++ build/index.php
@@ -3,7 +3,24 @@
         <!-- INDEX -->
         <section id="section--index" class="layout__page--index">
             <div class="layout__container--fixed-width">
-                Index Page
+            
+                <?php if(have_posts()): 
+                    while(have_posts()): the_post(); ?> 
+                        
+                        <article id="article-id-<?php the_ID(); ?>">
+                            <a href="<?php get_permalink('url'); ?>"><h2 class="post-title"><?php the_title(); ?></h2></a>
+                            <p><?php the_author(); ?> | <?php the_time(); ?></p>
+                            <p><?php the_excerpt(); ?></p>
+                            <hr>
+                            <p><?php the_content(); ?></p>
+                            <p><?php the_category(); ?> | <?php the_tags(); ?></p>
+                            <p><a href="<?php get_previous_post_link(); ?>">Previous Post</a> | <a href="<?php get_next_post_link(); ?>">Next Post</a></p>
+                        </article>
+
+                    <?php endwhile; 
+                endif; 
+                ?>
+
             </div>
         </section>
         <!-- end INDEX -->
